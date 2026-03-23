ecd305446a66dc883b9d396cbe0813eb94903fb0
Matheus Cristian <mathues@email.com>
2026-03-23 18:08:19 -0300

    feat: creates a session if it does not exist

--- src/index.js
+++ src/index.js
@@ -195,10 +195,21 @@ export default class WeniWebchatService extends EventEmitter {
       return;
     }
 
+    if (this.config.mode === 'preview') {
+      return;
+    }
+
     this._connecting = true;
 
     try {
-      const sessionId = this.session.getSessionId();
+      let sessionId = this.session.getSessionId();
+      if (!sessionId) {
+        sessionId = this.session.getOrCreate();
+        const currentSession = this.session.getSession();
+        if (currentSession) {
+          this.state.setSession(currentSession);
+        }
+      }
 
       const registrationData = {
         from: sessionId,
